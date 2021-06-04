9414eac74222a9c59ae0e5fb270e0cc814d4cc05
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-04 10:45:55 +0200

    cannon: Add hardware/xiaomi to the Soong namespaces
    Change-Id: I4bad251bc462199acea25ac6a8e33e71069c58f2
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -416,7 +416,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/cannon/cannon-vendor.mk)
