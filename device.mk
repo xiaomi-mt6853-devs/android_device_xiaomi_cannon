84799542b885063aa63aeca781ba8b09e644d712
AdarshGrewal <adarsh@aosip.dev>
2021-06-09 14:09:31 +0530

    cannon: Add RROs for setting correct device name
    Check device variant by using ro.boot.hwc and
    ro.boot.proudct.hardware.sku and set the correct model based on
    value of ro.product.marketname on stock ROM
    
    Change-Id: Iae15a7d3558f3c4608546e42ed655a76a0fe8868
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    CannonCNSettingsProviderOverlay \
+    CannonGLSettingsProviderOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Mediatek
