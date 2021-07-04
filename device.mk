9f57ad244107853f713d94ec45a94b0711923a83
Ash Blake <ash.blake@tutanota.com>
2021-07-04 10:53:20 +0000

    cannon: Build Mediatek FM Radio
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -269,6 +269,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FMRadio
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
