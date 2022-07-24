0e7b50fa39c474eb2ad3adae77fc9b09334f02db
bengris32 <bengris32@protonmail.ch>
2022-07-24 21:21:06 +0100

    cannon: power: Rewrite blueprint as makefile
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

---
+++ power/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power-service-mediatek
+LOCAL_VENDOR_MODULE := true
+LOCAL_MULTILIB := 64
+LOCAL_VINTF_FRAGMENTS := power-mediatek.xml
+LOCAL_SRC_FILES := Power.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder_ndk \
+    libpowerhal \
+    android.hardware.power-V1-ndk_platform
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+
+include $(BUILD_SHARED_LIBRARY)
