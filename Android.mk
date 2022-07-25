6b73140fd592b14e7242740707e9d09580f38721
Kshitij Gupta <kshitijgm@gmail.com>
2022-07-25 12:19:48 +0300

    cannong: Add vendor symlinks for vulkan and gatekeeper
    This is a squash of the following commits:
    
    https://github.com/StatiXOS/android_device_xiaomi_cannon/commit/a3aee7726b75dbc3c0e16b489bbf8db646733183
    https://github.com/StatiXOS/android_device_xiaomi_cannon/commit/f4a311eb33e8f6f12947db6574d4a9ad2e5b8e18
    https://github.com/StatiXOS/android_device_xiaomi_cannon/commit/4abd6109b4e5f2991b9ca217c8fae07664a81e6b
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),cannon)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6853.so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6853.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
