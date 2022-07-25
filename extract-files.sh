40480751cd1dc3f0c6ebf0247bc8cac6f7c82f48
bengris32 <bengris32@protonmail.ch>
2022-07-25 04:37:35 +0000

    cannon: Make mtkpower service part of `input` group
    * MediaTek's mtkpower service and aidl power service run in
      the same process (the aidl power service is a shared library)
    
    * Since we are using a custom aidl power implementation, make
      the mtkpower service part of the input group to ensure it
      can write to the DT2W nodes.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc)
+            echo "$(cat ${2}) input" > "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
