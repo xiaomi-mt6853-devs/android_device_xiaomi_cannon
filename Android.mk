b1dfa0a425b3a9890f2ba8c7f12701a304e6ba7b
LinkBoi00 <linkdevel@protonmail.com>
2022-08-09 16:01:46 +0300

    cannon: Drop some unused blobs and re-order sections
    * Drop 32 bit Gatekeeper blobs
    * Drop 64 bit Audio stack (libmiStereoFactoryRemapLib is needed for camera)
    * Drop 32 bit ConsumerIR blobs
    * Drop prebuilt Audio FX
    * Drop useless system_ext and system blobs
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- Android.mk
+++ Android.mk
@@ -17,7 +17,6 @@ $(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) echo "Making vendor symlinks"
 	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
 	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
 	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
 	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6853.so
 	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6853.so
