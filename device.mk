522fd7800297e316a895851468374b2902bf26bf
Robert Shih <robertshih@google.com>
2022-08-26 11:17:37 +0200

    cannon: Enable AIDL Clearkey HAL
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -162,7 +162,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
