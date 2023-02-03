3caf0565e9a897619cbe6f87cc7e1e16f4424e72
LinkBoi00 <linkdevel@protonmail.com>
2023-02-03 18:31:26 +0200

    cannon: Link camerahalserver service against VNDK 32 libutils
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
     vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
+    vendor/bin/hw/camerahalserver)
+        "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        ;;
     vendor/lib64/libmtkcam_featurepolicy.so)
         sed -i "s|\xE8\x87\x40\xB9|\x28\x02\x80\x52|g" "${2}"
         ;;
