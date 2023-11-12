3946ad82d420074eec7268af040a938a3b385998
Woomymy <woomy@woomy.be>
2023-11-12 13:36:18 +0100

    cannon: Link c2 media service against VNDK 33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    [LinkBoi00] 64 bits -> 32 bits. MTK moment.
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Elias Dimopoulos <linkdevel@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
     vendor/lib64/libcam.utils.sensorprovider.so)
         "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
         ;;
+    vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+        "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+        ;;
     esac
 }
 
